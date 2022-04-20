01ef1d291dc7a0f5f8722bd40c7bf2b607a03cc4
El-Glory <31097770+El-Glory@users.noreply.github.com>
2022-04-20 15:58:34 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,11 +8,9 @@ void _puts(char *str)
 {
 	int i;
 
-	while (str[i]) 
-        { 
-               _putchar​(str[i]); 
-               i++; 
-        } 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
 	_putchar('\n');
 }
-
